fe755f82ad10a54fe66251e01a13f2c606737254
Liyew <118590747+woletee@users.noreply.github.com>
2024-09-08 19:45:16 +0900

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def get_task(task_number):
         abort(404, description="Task not found")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5001)
 #
